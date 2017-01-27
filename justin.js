e7dc3a89984025e9fab3759937d1145ebbf777ce
Lediet <n_lediet@tss-hr420-dm06.fc.ca>
2017-01-27 09:10:12 -0500

    fun justin program

---
+++ justin.js
@@ -0,0 +1,66 @@
+
+class Door {
+    handle;
+    use() {
+        this.handle.turn();
+    }
+}
+
+class Justin extends People {
+   
+    isFacingDoor;
+    feet;
+    inChar = true;
+    width;
+    lean(bodytarget) {/* ... */ }
+    step(direction){/* ... */}
+    turn(degrees) {/* ... */}
+    stand (direction) { /* ... */}
+    step () {/* ... */}
+    pull(/* ... */)
+    useDoor(door) {
+        this.atDoor = true;
+        this.reach(door.target).use();
+        while (door.opening != this.width ) {
+            this.pull();
+            this.step(backwards);
+        }
+        this.pull(door);
+    }   
+}
+
+/* Position */
+
+var justin = new Justin();
+var door = new Door();
+
+/* Position */
+if( !Justin.isFacingDoor ) {
+    justin.turn(90.0); 
+}
+
+/* Stand */
+do {
+    justin.lean();
+    if (justin.isSecure) {
+        justin.stand();
+        justin.setinChair(true);
+    }    
+} while( justin.inChair)
+
+/* walking */
+while( !atDoor ){
+    justin.step(straight);
+} 
+
+/* leaving room */
+while( justin.inRoom ){
+    justin.step(straight);
+}
+
+
+door.addEventListener("collision", justin.useDoor, false);
+
+justin.addEventListener("outOfRoom", ()=>{
+    alert("Sucess");
+}, false);
